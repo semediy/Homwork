02387d05c2f7484736206accc351dd947ed6afde
semediy <c.semedii.yuliia@student.uzhnu.edu.ua>
2026-03-25 15:22:39 +0200

    3

--- Homwork/Form1.Designer.cs
+++ Homwork/Form1.Designer.cs
@@ -17,22 +17,133 @@ partial class Form1
         {
             components.Dispose();
         }
+
         base.Dispose(disposing);
     }
 
     #region Windows Form Designer generated code
 
     /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        button1 = new System.Windows.Forms.Button();
+        checkBox1 = new System.Windows.Forms.CheckBox();
+        label1 = new System.Windows.Forms.Label();
+        label2 = new System.Windows.Forms.Label();
+        label3 = new System.Windows.Forms.Label();
+        checkBox2 = new System.Windows.Forms.CheckBox();
+        textBox1 = new System.Windows.Forms.TextBox();
+        textBox2 = new System.Windows.Forms.TextBox();
+        label4 = new System.Windows.Forms.Label();
+        SuspendLayout();
+        // 
+        // button1
+        // 
+        button1.Location = new System.Drawing.Point(476, 151);
+        button1.Name = "button1";
+        button1.Size = new System.Drawing.Size(123, 33);
+        button1.TabIndex = 0;
+        button1.Text = "Порахувати";
+        button1.UseVisualStyleBackColor = true;
+        button1.Click += button1_Click_1;
+        // 
+        // checkBox1
+        // 
+        checkBox1.Location = new System.Drawing.Point(197, 216);
+        checkBox1.Name = "checkBox1";
+        checkBox1.Size = new System.Drawing.Size(104, 24);
+        checkBox1.TabIndex = 1;
+        checkBox1.Text = "checkBox1";
+        checkBox1.UseVisualStyleBackColor = true;
+        // 
+        // label1
+        // 
+        label1.Location = new System.Drawing.Point(206, 114);
+        label1.Name = "label1";
+        label1.Size = new System.Drawing.Size(58, 28);
+        label1.TabIndex = 3;
+        label1.Text = "label1";
+        // 
+        // label2
+        // 
+        label2.Location = new System.Drawing.Point(432, 263);
+        label2.Name = "label2";
+        label2.Size = new System.Drawing.Size(59, 32);
+        label2.TabIndex = 4;
+        label2.Text = "label2";
+        // 
+        // label3
+        // 
+        label3.Location = new System.Drawing.Point(590, 58);
+        label3.Name = "label3";
+        label3.Size = new System.Drawing.Size(98, 30);
+        label3.TabIndex = 5;
+        label3.Text = "label3";
+        // 
+        // checkBox2
+        // 
+        checkBox2.Location = new System.Drawing.Point(58, 136);
+        checkBox2.Name = "checkBox2";
+        checkBox2.Size = new System.Drawing.Size(104, 24);
+        checkBox2.TabIndex = 6;
+        checkBox2.Text = "checkBox2";
+        checkBox2.UseVisualStyleBackColor = true;
+        // 
+        // textBox1
+        // 
+        textBox1.Location = new System.Drawing.Point(63, 282);
+        textBox1.Name = "textBox1";
+        textBox1.Size = new System.Drawing.Size(100, 27);
+        textBox1.TabIndex = 7;
+        // 
+        // textBox2
+        // 
+        textBox2.Location = new System.Drawing.Point(411, 334);
+        textBox2.Name = "textBox2";
+        textBox2.Size = new System.Drawing.Size(188, 27);
+        textBox2.TabIndex = 8;
+        // 
+        // label4
+        // 
+        label4.Location = new System.Drawing.Point(387, 57);
+        label4.Name = "label4";
+        label4.Size = new System.Drawing.Size(100, 23);
+        label4.TabIndex = 9;
+        label4.Text = "label4";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(label4);
+        Controls.Add(textBox2);
+        Controls.Add(textBox1);
+        Controls.Add(checkBox2);
+        Controls.Add(label3);
+        Controls.Add(label2);
+        Controls.Add(label1);
+        Controls.Add(checkBox1);
+        Controls.Add(button1);
+        Text = "Form1";
+        ResumeLayout(false);
+        PerformLayout();
     }
+
+    private System.Windows.Forms.Label label4;
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.CheckBox checkBox2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.CheckBox checkBox1;
 
     #endregion
 }
--- Homwork/Form1.cs
+++ Homwork/Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
 namespace Homwork;
 
 public partial class Form1 : Form
@@ -5,5 +9,32 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+    }
+    
+
+    private void button1_Click_1(object sender, EventArgs e)
+    {
+        var controls = GetAllControls(this);
+
+        int total = controls.Count();
+
+        var grouped = controls
+            .GroupBy(c => c.GetType().Name)
+            .Select(g => $"{g.Key}: {g.Count()}");
+
+        string message = "Кількість елементів на формі: " + total + "\n\n";
+        message += string.Join("\n", grouped);
+
+        MessageBox.Show(message); 
+    }
+    private IEnumerable<Control> GetAllControls(Control parent)
+    {
+        foreach (Control control in parent.Controls)
+        {
+            yield return control;
+
+            foreach (var child in GetAllControls(control))
+                yield return child;
+        }
     }
 }
